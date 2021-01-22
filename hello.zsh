621a0d9b5db42b63bb4b347130d34a9d79177a7f
Rammy <kuttaineh@gmail.com>
2021-01-21 19:21:58 -0500

    Update hello.zsh
    Changed wording of greeting middle sentence.

--- hello.zsh
+++ hello.zsh
@@ -2,6 +2,6 @@
 comm() { print $1; say $1; }
 comm "hi there!"
 
-comm "My name is $0. I see your username is $USER. What shall I call you?"
+comm "My name is $0. I see you login as $USER. What shall I call you?"
 read name
 comm "Hello $name"
