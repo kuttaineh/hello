73df45b7cb5b0b3bc8fb5ed064acc364701161dd
Rammy <kuttaineh@gmail.com>
2021-08-08 10:35:32 -0400

    Update hello.zsh
    changed the noun 'login' to the acting verb 'log in'

--- hello.zsh
+++ hello.zsh
@@ -2,6 +2,6 @@
 utter() { print $1; say $1; }
 utter "hi there!"
 
-utter "My name is $0. I see you login as $USER. What shall I call you?"
+utter "My name is $0. I see you log in as $USER. What shall I call you?"
 read name
 utter "Hello $name"
