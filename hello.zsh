32c67e2f7f9b467d29f4b829f53b4dba0be6ddff
Rammy <kuttaineh@gmail.com>
2024-12-11 18:41:08 -0500

    Update hello.zsh
    Added " and we're upon $HOST" after $USER

--- hello.zsh
+++ hello.zsh
@@ -2,6 +2,6 @@
 utter() { print $1; say $1; }
 utter "Hi there!"
 
-utter "My name is $0. I see you log in as $USER. What shall I call you?"
+utter "My name is $0. I see you log in as $USER and we're upon $HOST. What shall I call you?"
 read name
 utter "Hello $name"
